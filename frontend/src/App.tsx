1bfb62458816775ac6f51ddb5deaebacb4f6d1a6
Szymon Muszyński <s.muszynski2003@gmail.com>
2025-06-01 00:27:41 +0200

    Add reservations data grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,9 @@ import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import MainPage from './pages/MainPage/MainPage.tsx'
 import CarAddPage from './pages/CarAddPage/CarAddPage.tsx'
 import Navbar from './components/Navbar/Navbar'
-import { mainPagePath, loginPagePath, carAddPagePath, usersPagePath } from './shared/pagesPaths.ts';
+import { mainPagePath, loginPagePath, carAddPagePath, usersPagePath, reservationsPagePath } from './shared/pagesPaths.ts';
 import UsersPage from './pages/UsersPage/UsersPage.tsx';
+import ReservationPage from './pages/ReservationPage/ReservationPage.tsx';
 
 function AppContent() {
   const location = useLocation();
@@ -16,6 +17,7 @@ function AppContent() {
         <Route path={mainPagePath} element={<MainPage />} />
         <Route path={carAddPagePath} element={<CarAddPage />} />
         <Route path={usersPagePath} element={<UsersPage/>} />
+        <Route path ={reservationsPagePath} element={<ReservationPage />} />
       </Routes>
     </>
   )
--- frontend/src/shared/pagesPaths.ts
+++ frontend/src/shared/pagesPaths.ts
@@ -3,3 +3,4 @@ export const loginPagePath = "/login";
 export const carAddPagePath = "/car-add";
 export const carEditPagePath = "/car-edit";
 export const usersPagePath = "/users";
+export const reservationsPagePath = "/reservations";
